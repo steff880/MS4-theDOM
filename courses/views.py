97c81631b1ae1e7228401aca075ab15f4b6a9fe1
steff880 <stefan.d.yordanov@gmail.com>
2021-11-09 16:35:17 +0000

    Add courses views and templates

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Course
 
-# Create your views here.
+
+def all_courses(request):
+    """
+    A view to show all courses,
+    inluding sorting and search queries
+    """
+
+    courses = Course.objects.all()
+
+    context = {
+        'courses': courses,
+    }
+
+    return render(request, 'courses/courses.html', context)
