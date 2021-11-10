d00e66ee1feb90f1ea5e3e2f6ff9af904dbeb85b
steff880 <stefan.d.yordanov@gmail.com>
2021-11-10 14:38:52 +0000

    Add course details functionality

--- courses/urls.py
+++ courses/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.all_courses, name='courses')
+   path('', views.all_courses, name='courses'),
+   path('<course_id>/', views.course_detail, name='course_detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 
 
@@ -15,3 +15,17 @@ def all_courses(request):
     }
 
     return render(request, 'courses/courses.html', context)
+
+
+def course_detail(request, course_id):
+    """
+    A view to show individual course details
+    """
+
+    course = get_object_or_404(Course, pk=course_id)
+
+    context = {
+        'course': course,
+    }
+
+    return render(request, 'courses/course_detail.html', context)
