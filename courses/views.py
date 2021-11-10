c44e0d2e456ed0bc7016f1384b160b859abe3f19
steff880 <stefan.d.yordanov@gmail.com>
2021-11-10 15:32:13 +0000

    Add sorting functionality

--- courses/views.py
+++ courses/views.py
@@ -14,8 +14,23 @@ def all_courses(request):
     courses = Course.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                courses = courses.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            courses = courses.order_by(sortkey)
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -32,10 +47,14 @@ def all_courses(request):
             queries = Q(name__icontains=query) | Q(
                 description__icontains=query)
             courses = courses.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'courses': courses,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'courses/courses.html', context)
