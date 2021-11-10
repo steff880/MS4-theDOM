647d08706aba52b3d42fe2b8a0492e092fb2d99c
steff880 <stefan.d.yordanov@gmail.com>
2021-11-10 14:56:55 +0000

    Add search functionality

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
+
 from .models import Course
 
 
@@ -9,9 +12,22 @@ def all_courses(request):
     """
 
     courses = Course.objects.all()
+    query = None
 
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, 'You did not enter any search criteria!')
+                return redirect(reverse('courses'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            courses = courses.filter(queries)
     context = {
         'courses': courses,
+        'search_term': query,
     }
 
     return render(request, 'courses/courses.html', context)
