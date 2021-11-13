db9ff6bcf47428d4efe3b60d8959ffac471bbe46
steff880 <stefan.d.yordanov@gmail.com>
2021-11-13 19:22:00 +0000

    Add more messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404)
 from django.contrib import messages
 
 from courses.models import Course
@@ -16,13 +17,14 @@ def add_to_bag(request, item_id):
     to the shopping bag
     """
 
-    course = Course.objects.get(pk=item_id)
+    course = get_object_or_404(Course, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {course.name}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {course.name}')
@@ -38,14 +40,17 @@ def adjust_bag(request, item_id):
     to the specified amount
     """
 
+    course = get_object_or_404(Course, pk=item_id)
     quantity = int(request.POST.get('quantity'))
 
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {course.name}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {course.name}')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -57,12 +62,15 @@ def remove_from_bag(request, item_id):
     shopping bag
     """
     try:
+        course = get_object_or_404(Course, pk=item_id)
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
+        messages.success(request, f'Removed {course.name}')
 
         request.session['bag'] = bag
 
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
