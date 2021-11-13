2fd2dd1a292375d386bacfae44fd090c52b93381
steff880 <stefan.d.yordanov@gmail.com>
2021-11-13 13:32:53 +0000

    Add remove from bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
@@ -44,3 +44,20 @@ def adjust_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """
+    Remove the item from the
+    shopping bag
+    """
+    try:
+        bag = request.session.get('bag', {})
+
+        bag.pop(item_id)
+
+        request.session['bag'] = bag
+
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
