deffdaa5a6eed79080c695773a2e3a1f4674c40e
steff880 <stefan.d.yordanov@gmail.com>
2021-11-14 16:24:49 +0000

    Create checkout app and models

---
+++ checkout/models.py
@@ -0,0 +1,83 @@
+import uuid
+
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from courses.models import Course
+
+
+class Order(models.Model):
+    """ Order Model """
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    discount = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for discount.
+        """
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total < settings.DISCOUNT_THRESHOLD:
+            self.discount = (
+                self.order_total * settings.DISCOUNT_PERSENTAGE / 100
+            )
+        else:
+            self.discount = 0
+        self.grand_total = self.order_total - self.discount
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it has not been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItems(models.Model):
+    """
+    OrderLineItems Model
+    """
+    order = models.ForeignKey(
+        Order, null=False, blank=False,
+        on_delete=models.CASCADE, related_name='lineitems')
+    course = models.ForeignKey(
+        Course, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False,
+        blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.course.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'ID {self.course.id} on order {self.order.order_number}'
--- the_dom/settings.py
+++ the_dom/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'home',
     'courses',
     'bag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
