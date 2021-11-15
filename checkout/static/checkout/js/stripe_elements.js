f8e4a46102104a4331c1e97449b7967bb62d3840
steff880 <stefan.d.yordanov@gmail.com>
2021-11-15 15:13:14 +0000

    Add js to handle realtime validation errors on the card element

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -32,3 +32,19 @@ var card = elements.create('card', {
     style: style
 });
 card.mount('#card-element');
+
+// Handle realtime validation errros on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
