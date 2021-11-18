a2a0e17c6796eac2fe3d94fec19a3c132a6e01a5
steff880 <stefan.d.yordanov@gmail.com>
2021-11-18 18:59:50 +0000

    Deployment part 2

--- the_dom/settings.py
+++ the_dom/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-v79w&=r^^vo7^cz7osd0f3#&d@suzn8pk05em2*bwc=l#814!6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['steff880-the-dom.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'the_dom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
