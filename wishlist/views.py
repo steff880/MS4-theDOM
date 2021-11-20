76b136d6f43d24b42aafd39f33f3d83e5d7e2a87
steff880 <stefan.d.yordanov@gmail.com>
2021-11-20 08:42:41 +0000

    Add wishlist view, url and template

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import WishList
+
+
+@login_required
+def wishlist(request):
+    """
+    A view to render the user's wishlist
+    """
+    wishlist = None
+    try:
+        wishlist = WishList.objects.get(user=request.user)
+    except WishList.DoesNotExist:
+        pass
+
+    context = {
+        'wishlist': wishlist,
+    }
+
+    return render(request, 'wishlist/wishlist.html', context)
